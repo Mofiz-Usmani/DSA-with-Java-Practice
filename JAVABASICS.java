9fcccbcb3db3b8f1d2412dd6dae1534cbad232f7
Jackal <mofizusmani1@gmail.com>
2025-09-13 13:05:00 +0530

    Initial commit

---
+++ JAVABASICS.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class JAVABASICS {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Your name : ");
+        // String c = sc.nextLine();
+
+        // System.out.println("Enter your Age : ");
+        // int age = sc.nextInt();
+
+        // System.out.println("Enter you CGPA : ");
+        // float cgpa = sc.nextFloat();
+
+
+        // System.out.println(c + " is " + age + " years " + "old" + " having "+ cgpa + " CGPA");
+
+        // Int to long Possible
+        // int a = 25;
+        // long b = a;
+
+        // Long to Int NOt Possible
+        // long a = 25;
+        // int b = a;
+
+        // INt to float 
+        float number = sc.nextFloat();
+        System.out.println(number);
+
+    }
+}
