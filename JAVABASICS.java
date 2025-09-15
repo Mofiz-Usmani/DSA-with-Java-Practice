3c6d4b3910a2f966cdfe730c1777ef9070ae5955
Jackal <mofizusmani1@gmail.com>
2025-09-15 22:46:51 +0530

    ternary operator

--- JAVABASICS.java
+++ JAVABASICS.java
@@ -56,22 +56,35 @@ public class JAVABASICS {
 
         // Write a Java program that takes three integers as input-
         // and prints the largest number using only if–else statements (no built-in Math.max).
+        // System.out.println("Enter a : ");
+        // int a = sc.nextInt();
+        // System.out.println("Enter b : ");
+        // int b = sc.nextInt();
+        // System.out.println("Enter c : ");
+        // int c = sc.nextInt();
+
+
+        // if( a>b && a>c) {
+        //     System.out.println("a is greater" + " a : "+a);
+        // }
+        // else if(b>a && b>c){
+        //     System.out.println("b is greater" + " b : "+b);
+        // }
+        // else {
+        //     System.out.println("c is greater" + " c : "+c);
+        // }
+
+
+
         System.out.println("Enter a : ");
         int a = sc.nextInt();
         System.out.println("Enter b : ");
         int b = sc.nextInt();
         System.out.println("Enter c : ");
         int c = sc.nextInt();
-
-
-        if( a>b && a>c) {
-            System.out.println("a is greater" + " a : "+a);
-        }
-        else if(b>a && b>c){
-            System.out.println("b is greater" + " b : "+b);
-        }
-        else {
-            System.out.println("c is greater" + " c : "+c);
-        }
+        int largest = (a > b) 
+                ? (a > c ? a : c) 
+                : (b > c ? b : c);
+        System.out.println("Largest number is " + largest);
     }
 }
