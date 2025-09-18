1d93e930ab4bd4be5a21b6388baf34d90560bb7d
Jackal <mofizusmani1@gmail.com>
2025-09-18 12:33:03 +0530

    Pattern 5

--- JAVABASICS.java
+++ JAVABASICS.java
@@ -203,11 +203,26 @@ public class JAVABASICS {
 
 
         // Pattern 4
-        char ch = 'A';
-        for(int i=0; i<=4; i++){
-            for(int j=1; j<=i; j++){
-                System.out.print(ch);
-                ch++;
+        // char ch = 'A';
+        // for(int i=0; i<=4; i++){
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print(ch);
+        //         ch++;
+        //     }
+        //     System.out.println();
+        // }
+
+
+
+
+        // Pattern 5
+        for(int i=1; i<=4; i++){
+            for(int j=1; j<=5; j++){
+                if(i == 1 || i == 4 || j == 1 || j == 5){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
