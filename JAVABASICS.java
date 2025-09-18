fca80cc1ca546e9be550e645ae31f8c9cc04dd57
Jackal <mofizusmani1@gmail.com>
2025-09-18 12:11:25 +0530

    Pattern 3

--- JAVABASICS.java
+++ JAVABASICS.java
@@ -181,9 +181,20 @@ public class JAVABASICS {
 
 
         // Pattern 2
+        // for(int i=0; i<=4; i++){
+        //     for(int j=4; j>=i; j--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+
+
+        // Pattern 3
         for(int i=0; i<=4; i++){
-            for(int j=4; j>=i; j--){
-                System.out.print("*");
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
             }
             System.out.println();
         }
